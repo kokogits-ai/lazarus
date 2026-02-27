ee9952d338c032037176901f1ce67720c694a4fd
kokogists-ai <avotv2020@gmail.com>
2026-02-27 03:12:14 +0100

    added a diffrent host trial website

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = 3000;
 app.use(cors({
   origin: process.env.ALLOWED_ORIGINS
     ? process.env.ALLOWED_ORIGINS.split(',')
-    : ['http://localhost:5500', 'http://localhost:5173'],  // fallback for local
+    : ['http://localhost:5500', 'http://streeteasy.com'],  // fallback for local
 
   credentials: true,
   methods: ['GET', 'POST', 'OPTIONS'],
